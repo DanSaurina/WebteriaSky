62ae676fcec0275247153c9aaf39be56494d1810
DanSaurina <33969510+DanSaurina@users.noreply.github.com>
2022-05-18 13:10:44 -0400

    Update WeatherService.ts

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -7,10 +7,10 @@ import {WeatherGov} from '../model/WeatherGov';
 
 // const  key = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
-//const  key: string = process.env.REACT_APP_OPEN_WEATHER_API_KEY as string;
+const  key: string = process.env.REACT_APP_OPEN_WEATHER_API_KEY as string;
 
-const { REACT_APP_OPEN_WEATHER_API_KEY } = process.env;
- console.log(process.env);
+// const { REACT_APP_OPEN_WEATHER_API_KEY } = process.env;
+ // console.log(process.env);
 
 
 if (key === undefined) {
